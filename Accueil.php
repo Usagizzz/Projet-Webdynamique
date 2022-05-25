31071439bd32be8414fb2ab37a87958ae47cb4a4
Usagizzz <94546936+Usagizzz@users.noreply.github.com>
2022-05-25 08:34:36 -0400

    Update Accueil.php

--- Accueil.php
+++ Accueil.php
@@ -1,76 +1,131 @@
 <!DOCTYPE html>
 <html>
-	<head>
-    		 <title>Accueil</title>
-             <link rel="stylesheet" type="text/css" href="accueilcss.css">
-             <link rel="stylesheet" type="text/css" href="testchatcss.css">
-             <meta charset="utf-8" />
+<head>
+  <title>Accueil</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>    
+    
+   #apropos .contient{
+    padding: 70 px 10px;
+    max-width: 1500px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: row;
+}
+    #apropos .contient .gauche{
+        width: 70%;
+        background-color: lightblue;
+         border-radius: 5px;
+    }
+    #apropos .contient .gauche h3{
+        font-size: 2em;
+        color: ghostwhite;
+    }
+    #apropos .contient .droite{
+        width: 30%;
+        background-color: ghostwhite;
+         border-radius: 5px;
 
-    </head>
-    <body>
+    }
+    #apropos .contient .droite h3{
+        font-size: 2em;
+        color: dimgray;
+}
+.gestionrdv ul li{
+   
+    list-style: none;
+    display: inline-block;
+    padding: 25px;
+     float: left;
+}
+.gestionrdv ul li h3{
+    color: black;
+}
+.gestionrdv ul li button{
+    border-radius: 15px;
+    border: none;
+}
+.gestionrdv ul li img{
+    border-radius: 15px;
+    border: none;
+}
 
-      <div class="wrapper">
+.gestionrdv ul li:hover{
+    transform: translateX(25px);
+}
+    iframe{
+      height: 100%;
+      width: 100%;
+    }
 
-       <header>
+    footer {
+      background-color: #555;
+      color: white;
+      padding: 15px;
+    }
+    header{
+    color: dimgray;
+    height: 50vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background:linear-gradient(to left top, whitesmoke, lightblue);
+
+}
+header h1{
+    font-size: 90px;
+}
+  </style>
+</head>
+<body>
+   <header>
         <h1>OMNES SANTE</h1>
         <h3><em>Pour une humanité plus saine</em></h3>
       </header>
-        
-        <div id="nav" class="navigation">
-            <ul>
-                <li> <a href="accueil.html">  
-                    <span class="icone">
-                <img src="home.png" alt="Home" height="40" width="40"> </span>
-                 <span class="txt">Accueil </span>  </a>         </li> 
 
-                 <li> <a href="specialiste.html">  
-                    <span class="icone">
-                <img src="parcourir.png" alt="parcourir" height="40" width="40"> </span>
-                 <span class="txt">Tout parcourir </span>  </a>         </li> 
-
-                   <li> <a href="login.html"> 
-                    <span class="icone">
-                <img src="authen.png" alt="Authentification" height="40" width="40">   </span>
-                <span class="txt">Recherche </span>  </a></li> 
-          
-                  <li> <a href="login.html"> 
-                    <span class="icone">
-                <img src="calendar.png" alt="Authentification" height="40" width="40">   </span>
-                <span class="txt">Rendez-vous </span>  </a></li> 
-          
-                  <li> <a href="login.html"> 
-                    <span class="icone">
-                <img src="motde.png" alt="Authentification" height="40" width="40">   </span>
-                <span class="txt">Votre compte </span>  </a></li> 
-           
-                <form class="connexion"> 
-                    <input type="text" name="nomuser" placeholder="Nom d'utilisateur">
-                    <input type="password" name="mdpuser" placeholder="Mot de passe">
-                    <button>Connexion</button>
-                   <!-- <input type="search" name="search" placeholder="Recherche">-->
-                </form>
-            </ul>
-               
-            </div><!--fin navigation-->
-     
-      <div id="caroussel">
-               <div class="images">
-                      <ul>
-         <li><img src="teamsante.png" width="300" height="200" /></li>
-         <li><img src="chirurgien.png" width="300" height="200" /></li>
-         <li><img src="ophtalmo.png" width="300" height="200" /></li>
-         <li><img src="dermato.png" width="300" height="200" /></li>
-                </ul>
-               
-             </div>
-    </div> 
-    <!--A propos-->
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="accueil.html">Accueil</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="rdv.html">Rendez-vous </a></li>
+        <li><a href="specialiste.html">Tout parcourir</a></li>
+      </ul>
+      <form class="navbar-form navbar-right" role="search">
+        <div class="form-group input-group">
+          <input type="text" class="form-control" placeholder="Search..">
+          <span class="input-group-btn">
+            <button class="btn btn-default" type="button">
+              <span class="glyphicon glyphicon-search"></span>
+            </button>
+          </span>        
+        </div>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="login.html"><span class="glyphicon glyphicon-user"></span> Votre compte</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!--A propos-->
     <section id="apropos">
         <div class="contient">
             <div class="gauche">
             <h3><em>&nbsp;&nbsp;A propos</em></h3>
             <div>
-                   <p> &nbsp;&nbsp;&nbsp; Ce site innovant permette au client de voir une liste de tous les personnelles de Omnes Santé, de sélectionner un spécialiste préféré, de voir ses informations pertinentes, tels que son CV, ses coordonnées, et son calendrier de la semaine ainsi que sa disponibilité pour recevoir un rendez-vous.</p> 
+                   <p> &nbsp;&nbsp;&nbsp; Institué en 2022, OMNES SANTE  représente la branche centrale du domaine médicale du groupe OMNES. Ce site innovant regroupe une liste de tous les personnels de OMNES SANTE, de sélectionner un spécialiste préféré, de voir ses informations pertinentes, tels que son CV, ses coordonnées, et son calendrier de la semaine ainsi que sa disponibilité pour recevoir un rendez-vous.</p> 
                    <p>&nbsp;&nbsp;&nbsp;Quand un rendez-vous avec ce spécialiste est fait, le client reçoit une confirmation de ce rendez-vous. Omnes Santé est ouvert à tous les membres de la communauté Omnes Education</p> 
             </div><!--fin paragraphes-->
             </div><!--fin gauche-->
@@ -87,63 +142,63 @@
         </div><!--fin contient-->
 
     </section>
-
-     <!--Interactions clients-->
+  
+<div class="container text-center">    
+  <div class="row">
+    <div class="col-sm-3 well">
+      <div class="well">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.3377133610643!2d2.281997230171113!3d48.85177024288572!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6701b4f58251b%3A0x167f5a60fb94aa76!2sECE%20Paris%20Lyon!5e0!3m2!1sfr!2sfr!4v1653430068691!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+      <div class="well">
+        <p><a href="#">Notre équipe</a></p>
+        <p>
+          <span class="label label-default">Justine BOUDOUX</span>
+          <span class="label label-primary">Minh-Bao Potier</span>
+          <span class="label label-success">Thibaud PERRET</span>
+          <span class="label label-info">Clarence MAGNE</span>
+        </p>
+      </div>
+      <div class="alert alert-success fade in">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+        <p><strong>Bulletin santé de la semaine</strong></p>
+        <ul>
+          <li><em>Nouvelle maladie enregistrée sur Paris : Variole du singe</em></li>
+          <li><em>Diminution progressive du COVID-19 ? On en parle </em></li>
+          <li><em>Collecte de sang à Rouen : venez sauver une vie comme Wanda</em></li>
+        </ul>
+      </div>
+         <p style="color: dimgray;"> <strong>Evenements de la semaine </strong></p>
+        <ul>
+          <li>Portes ouvertes OMNES SANTE : 1 mai 2022</li>
+          <li>Séminaire sur la cafet de E2 </li>
+          <li>Rencontre avec l'administration du groupe OMNES</li>
+        </ul>
+    </div>
+    <div class="col-sm-7">
+    
+        <!--Interactions clients-->
     <div class="gestionrdv">
 
-            <ul>
+            <tr>
         
-            <li> <h3>Prendre un rendez-vous</h3>
-            <a href="rdv.html"> <button type="button"> <img src="rdvfond.png" name="fondrdv" alt="image_sympa" height="200" width="200"></button> </a> </li>
+            <td> <h3>Prendre un rendez-vous</h3>
+            <a href="rdv.html"> <button type="button"> <img src="rdvfond.png" name="fondrdv" alt="image_sympa" height="200" width="200"></button> </a> </td>
         
       
-           <li> <h3>Historique des consultations</h3>
-            <a href=""> <button type="button"> <img src="historique.png" height="200" width="200"> </button> </a> </li>
-          </ul>
+           <td> <h3>Historique des consultations</h3>
+            <a href=""> <button type="button"> <img src="historique.png" height="200" width="200"> </button> </a> </td>
+          </tr>
    
     </div> <!--fin gestionrdv-->
-    <div class="map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.3377133610643!2d2.281997230171113!3d48.85177024288572!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6701b4f58251b%3A0x167f5a60fb94aa76!2sECE%20Paris%20Lyon!5e0!3m2!1sfr!2sfr!4v1653430068691!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
-    </div>
-
     
-<!--test chatbox-->
-<!--
- <div class="chat-bar-collapsible">
-        <button id="chat-button" class="collapsible" type="button"> Contactez-nous</button>
-        <div class="content">
-            <div class="full-chat-block">
-                <div class="outer">
-                    <div class="chat-container">
-                        <div id=chatbox>
-                            <h5 id="timestamp"></h5>
-                            <p id="botStarterMessage" class="botText"><span>Chargement ...</span></p>
-                        </div> 
-                    --> <!--fin chatbox-->
-<!--
-                       <div class="chat-bar">
-                        <div id="userInput">
-                            <input type="text" name="msg" id="textInput" clas="inputt-box" placeholder="Appuyer Entrer pour envoyer un message">
-                            <p>
-                            </p>
+     </div>
+</div>
 
-                        </div>
-                    --> <!--fin userInput-->
-                      <!-- </div>--> <!--fin chatbar--> 
-                    <!--   <div id="chat-bar-bottom">
-                            <p></p>
+<footer class="container-fluid text-center">
+  <p><em>Nous contacter :</em></p>
+  <p>Tel: +33 01 02 03 04 05</p>
+  <p>Courriel : administration@omnessante.fr</p>
+</footer>
 
-                       </div>
-                     </div> --> <!--fin chat-container-->
-               <!--  </div>  -->  <!--fin outer-->
-           <!-- </div> --> <!--fin full chat block-->
-
-    <!--    </div>
-
-    </div>-->
-  <!--fin test chatbox-->  
-     </div> <!--fin wrapper-->              
-    </body>
-
-</html>    
+</body>
+</html>
