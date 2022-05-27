aebbba947d82a73b8fb5fe3e53acee1c330a3d3f
minhbaopotier <81973327+minhbaopotier@users.noreply.github.com>
2022-05-27 14:21:06 +0200

    Update loginadmin.php

--- loginadmin.php
+++ loginadmin.php
@@ -1,16 +1,16 @@
 <?php
 echo "<meta charset=\"utf-8\">";
 //echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"login.css\">";
-$nom=isset($_POST['nom'])?$_POST['nom']:"";
-$prenom=isset($_POST['prenom'])?$_POST['prenom']:"";
-$courriel=isset($_POST['courriel'])?$_POST['courriel']:"";
-$mdp=isset($_POST['mdp'])?$_POST['mdp']:"";
+$nom=isset($_POST["nom"])?$_POST["nom"]:"";
+$prenom=isset($_POST["prenom"])?$_POST["prenom"]:"";
+$courriel=isset($_POST["courriel"])?$_POST["courriel"]:"";
+$mdp=isset($_POST["mdp"])?$_POST["mdp"]:"";
 $database="projet webdynamique";
 
 $db_handle = mysqli_connect('localhost:3307', 'root', '');
 
 $db_found = mysqli_select_db($db_handle, $database);
-
+echo "dbfound=1<br>";
 if (isset($_POST["button1"])){
 	echo "connecter";
 	if($db_found)
@@ -23,7 +23,7 @@ if (isset($_POST["button1"])){
 			$result=mysqli_query($db_handle,$sql);
 			if(mysqli_num_rows($result) == 0)
 			{
-			echo "Erreur.<br>";
+			echo "Erreur:requête vide.<br>";
 			}
 			else
 			{
@@ -33,12 +33,16 @@ if (isset($_POST["button1"])){
 		}
 		else
 		{
-			echo "Erreur";
+			echo "Erreur: pas de nom";
 		}
 
+	}
+	else
+	{
+	echo "Erreur: base injoignable";
+	}
 }
 else
 {
-	echo "db_found=0";
-}
+	echo "pas de bouton pressé";
 }
