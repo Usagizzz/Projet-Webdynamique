3d42458450ad29fef20e9aa5e96b86dee28fe757
minhbaopotier <81973327+minhbaopotier@users.noreply.github.com>
2022-05-27 12:27:50 +0200

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,41 @@
+    <!DOCTYPE html>
+<html>
+    <head>
+             <title>Accueil</title>
+             <link rel="stylesheet" type="text/css" href="accueilcss.css">
+             <meta charset="utf-8" />
+    </head>
+    <body>
+        <div id="wrapper">
+
+        <div class="header" id="entete">
+            <center> <img src="omnes.png"> </center>
+
+
+        </div>
+    <div id="nav" class="navigation">
+            <ul>
+                <li> <a href="localhost:8080/inscriptionmedecin.html">  
+                    
+                 Ajout médecin   </a>         </li> 
+
+                 <li> <a href="accueil.html">  
+                   
+                 Suppression médecin   </a>         </li> 
+
+                   <li> <a href="login.html"> 
+                    
+                Fichiers xml   </a></li> 
+          
+                  <li> <a href="login.html"> 
+                    
+
+                Vérification disponibilité  </a></li> 
+          
+                  
+                </form>
+            </ul>
+               
+            </div><!--fin navigation-->
+        </div>
+        </body>
