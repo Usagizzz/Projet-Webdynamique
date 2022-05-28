73058b3d8da34c852c7bdef8354929bf26a728c4
minhbaopotier <81973327+minhbaopotier@users.noreply.github.com>
2022-05-28 16:04:16 +0200

    Create suppressionmedecin.php

---
+++ suppressionmedecin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    		 <title>Suppression médecin</title>
+    		<!-- <script type="text/javascript" src="loginform.js"> </script>-->
+    		<link rel="stylesheet" type="text/css" href="logincss.css">
+   		  <meta charset="utf-8" />
+    </head>
+
+    <body>
+    	<center>
+            <div id="informations">
+        <form enctype="multipart/form-data" action="medecin_suppr.php" method="post" class="text-center">
+
+        <tr> <td> <b>Saisir ID à supprimer:</b></td></tr>
+
+        <tr> <td> <input type="text" placeholder ="Entrer l'ID'" class="inputbox" name="ID"/> </td> </tr>
+        <button name="submit" type="submit" style="width: 95px;border-radius: 5px;">Supprimer</button> <br>
+
+            <br>
+
+
+</center>
+</div>
+</body>
+</html>
