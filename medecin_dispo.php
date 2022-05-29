c6283e19fac8af553475b11ec9cf9cc53931b331
minhbaopotier <81973327+minhbaopotier@users.noreply.github.com>
2022-05-29 13:03:41 +0200

    Update medecin_dispo.php

--- medecin_dispo.php
+++ medecin_dispo.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" type="text/css" href="accueilcss.css">
 <?php
 
 $database = 'projetweb';
@@ -28,13 +29,16 @@ $db_found = mysqli_select_db($db_handle, $database);
                 	echo "<th>" . "Prénom du patient" . "</th>";
                 	echo "</tr>";
                 	while ($data = mysqli_fetch_assoc($result)) {
-					echo "Nom du médecin: " . $data['docnom'] . '<br>';
- 					echo "Créneau pris:" . $data['heure'] . '<br>';
- 					echo "Date: " . $data['date'] . '<br>';
- 					echo "Lieu: " . $data['lieu'] . '<br>';
- 					echo "Nom du patient: " . $data['nompatient'] . '<br>';
-                	echo "Prénom du patient: " . $data['prenompatient'] . '<br>';
+                		echo "<tr>";
+					echo "<td>" . $data['docnom'] . '</td>';
+ 					echo "<td>" . $data['heure'] . '</td>';
+ 					echo "<td>" . $data['date'] . '</td>';
+ 					echo "<td>" . $data['lieu'] . '</td>';
+ 					echo "<td>" . $data['nompatient'] . '</td>';
+                			echo "<td>" . $data['prenompatient'] . '</td>';
+                	echo "</tr>";
                 	}
+                	echo "</table>";
                 	echo "<a href='admin.php'>Retour à l'admin</a>";
 
             	}
